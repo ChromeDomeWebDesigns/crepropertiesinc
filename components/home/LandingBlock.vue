3511c2f725bd79b1a5c7a90dab690754889b7971
Zachary Cuddy <info@chromedomewebdesigns.com>
2024-08-17 16:01:20 -0500

    landing done

--- components/home/LandingBlock.vue
+++ components/home/LandingBlock.vue
@@ -2,12 +2,12 @@
   <main class="flex column flex-center">
     <div class="page-margin w-100">
       <div class="landing-square">
-        <section class="flex column content">
+        <section class="flex column">
           <h1>Family First Real Estate Investing</h1>
           <p>Located in St. Louis, MO we are dedicated to treating our clients and tenants as one of our own.</p>
         </section>
-        <div class="flex align-center mt-2">
-          <nuxt-link to="/properties" class="flex align-center btn-link mr-1 alt">View properties</nuxt-link>
+        <div class="button-container">
+          <nuxt-link to="/properties" class="flex align-center btn-link alt">View properties</nuxt-link>
           <nuxt-link to="/contact" class="flex align-center btn-link">Contact us</nuxt-link>
         </div>
       </div>
@@ -31,83 +31,109 @@
     background-color: color('primary');
     background-image: url('/images/Landing.jpg');
     background-size: cover;
-    background-position: center;
+    background-position: 75% center;
 
     .landing-square {
-      margin-top: 5rem;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
       text-align: center;
       background: rgba(color('primary'), 90%);
-      width: 40vw;
-      padding: 3rem 1rem;
+      width: auto;
+      padding: 2rem 2rem;
+      font-size: 1em;
 
-      .content {
-        font-size: 1em;
+      @media (min-width: breaks(phone)) {
+        font-size: 1.1em;
+      }
+
+      @media (min-width: breaks(phablet)) {
+        font-size: 1.3em;
+      }
+
+      @media (min-width: breaks(small-tablet)) {
+        margin-top: 5rem;
+        width: 50vw;
+      }
+
+      @media (min-width: breaks(tablet)) {
+        font-size: 1.5em;
+      }
+
+      @media (min-width: breaks(large-tablet)) {
+        padding: 3rem 1rem;
+        width: 40vw;
+      }
+
+      @media (min-width: breaks(laptop)) {
+        font-size: 1.8em;
+      }
+
+      @media (min-width: breaks(desktop)) {
+        font-size: 2em;
+      }
+
+
+      h1 {
+        font-weight: 900;
+        font-size: 1.3em;
+        color: color('blackLight');
 
         @media (min-width: breaks(phone)) {
-          font-size: 1.1em;
-        }
-
-        @media (min-width: breaks(phablet)) {
-          font-size: 1.3em;
+          font-size: 1.4em;
         }
 
         @media (min-width: breaks(tablet)) {
           font-size: 1.5em;
         }
-
-        @media (min-width: breaks(laptop)) {
-          font-size: 1.8em;
-        }
-
-        @media (min-width: breaks(desktop)) {
-          font-size: 2em;
-        }
-
-        h1 {
-          font-weight: 900;
-          font-size: 1.3em;
-          color: color('blackLight');
-
-          @media (min-width: breaks(phone)) {
-            font-size: 1.4em;
-          }
-
-          @media (min-width: breaks(tablet)) {
-            font-size: 1.5em;
-          }
-        }
-
-        p {
-          color: color('white');
-          margin-top: 1rem;
-          font-size: 0.7em;
-        }
       }
 
-      .btn-link {
-        background-color: transparent;
+      p {
         color: color('white');
-        border-color: color('white');
-        padding: 0.75rem 1.5rem;
-        font-weight: 600;
-        font-size: 0.9em;
+        margin-top: 1rem;
+        font-size: 0.7em;
+      }
 
-        &.alt {
-          color: color('primary');
-          background-color: color('white');
-          border-color: color('white');
+      .button-container {
+        display: flex;
+        align-items: center;
+        margin-top: 1rem;
+
+        @media (min-width: breaks(small-tablet)) {
+          margin-top: 2rem;
         }
 
-        &:hover {
-          background-color: lighten(color('primary'), 10%);
-          border-color: lighten(color('primary'), 10%);
+       .btn-link {
+          background-color: transparent;
+          color: color('white');
+          border-color: color('white');
+          font-weight: 600;
+          font-size: 0.5em;
+          padding: 0.75rem 1rem;
+
+          @media (min-width: breaks(small-tablet)) {
+            padding: 0.75rem 1.5rem;
+          }
 
           &.alt {
-            color: color('white');
+            margin-right: 0.5rem;
+            color: color('primary');
+            background-color: color('white');
+            border-color: color('white');
+
+            @media (min-width: breaks(small-tablet)) {
+              margin-right: 1rem;
+            }
+          }
+
+          &:hover {
+            background-color: lighten(color('primary'), 10%);
+            border-color: lighten(color('primary'), 10%);
+
+            &.alt {
+              color: color('white');
+            }
           }
         }
       }
